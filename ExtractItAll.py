d51067db023bc4f78927fa7b213154cfbad3ecc2
Juan P. Abad <37944429+jabadgo@users.noreply.github.com>
2018-03-31 17:59:11 -0500

    Add files via upload
    Added user input

--- ExtractItAll.py
+++ ExtractItAll.py
@@ -1,15 +1,23 @@
 import os
 import zipfile
 
-filename = []
-filePath = []
+while True:
+    mydir = raw_input("Please write the path to where the files you want to extract are. \nPath:") #The directory where the .zip files are
+    print "Your path is:", mydir, "\n"
+    if mydir != " ":
+        break
+    else:
+        print "You didn't write a path, try again."
 
-mydir = ".../mydirA" #The directory where the .zip files are
-targetDir = ".../mydirB" #The directory in which you want to extract the files
+targetDir = raw_input("Please write the path to where you want to extract the files. Leave this empty if you want to use the source directory. \nPath:") #The directory in which you want to extract the files
+if targetDir == " ":
+    targetDir = mydir
+    print "Your extraction path is:", targetDir, "\n"
+
 
 for file in os.listdir(mydir):
-    if file.endswith(".zip"):
-        myPath = (os.path.join(mydir, file))
-        targetFile = file.split(".")
-        with zipfile.ZipFile(myPath,'r') as zip_ref:
+    if file.endswith(".zip"): #check filetype
+        filePath = (os.path.join(mydir, file))
+        targetFile = file.split(".") #remove the .zip suffix from the target filename
+        with zipfile.ZipFile(filePath,'r') as zip_ref:
             zip_ref.extractall(targetDir + "/" + targetFile[0])

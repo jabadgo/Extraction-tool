74fabe760524a4e67e56790a7035638c884872dc
Juan P. Abad <37944429+jabadgo@users.noreply.github.com>
2018-04-01 10:52:55 -0500

    Add files via upload
    Added more input options. Now it can extract from and to the where the .py file is located.

--- ExtractItAll.py
+++ ExtractItAll.py
@@ -1,13 +1,28 @@
 import os
 import zipfile
 
+currentDir = os.path.dirname(os.path.realpath(__file__)) #Getting the full path to the .py file
+print 'Welcome \n\n'
+print 'The current directory is:',currentDir,'\n'
+
+
 while True:
-    mydir = raw_input("Please write the path to where the files you want to extract are. \nPath:") #The directory where the .zip files are
-    print "Your path is:", mydir, "\n"
-    if mydir != " ":
+    askDir = raw_input("Do you want to use to use this directory? (Y/N) \n")
+    if askDir == 'Y':
+        mydir = currentDir
+        break
+    elif askDir == 'N':
+        while True:
+            mydir = raw_input("Please write the path to where the files you want to extract are. \nPath:") #The directory where the .zip files are
+            print "Your path is:", mydir, "\n"
+            if mydir != " ":
+                break
+            else:
+                print "You didn't write a path, try again."
         break
     else:
-        print "You didn't write a path, try again."
+        print "Sorry, I didn't get that one"
+        askDir = raw_input("Do you want to use to use this directory? (Y/N) \n")
 
 targetDir = raw_input("Please write the path to where you want to extract the files. Leave this empty if you want to use the source directory. \nPath:") #The directory in which you want to extract the files
 if targetDir == " ":
